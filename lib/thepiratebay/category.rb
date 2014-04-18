d408b82b918ade3ef6cdf85ead02e2394c0b263b
unmanbearpig <unmanbearpig@gmail.com>
2014-04-18 06:23:14 +0400

    Added Video categories

--- lib/thepiratebay/category.rb
+++ lib/thepiratebay/category.rb
@@ -1,9 +1,14 @@
 module ThePirateBay
   module Category
-    Audio          = "100"
-    Video          = "200"
-    Applications   = "300"
-    Games          = "400"
-    Other          = "600"
+    Audio                    = "100"
+    Video                    = "200"
+    Video_Movies             = "201"
+    Video_Movies_DVDR        = "202"
+    Video_Movies_HD_Movies   = "207"
+    Video_Movies_HD_TV_Shows = "208"
+    Applications             = "300"
+    Games                    = "400"
+    Other                    = "600"
+    All                      = "all"
   end
 end
