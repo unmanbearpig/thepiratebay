2bc5c20b1cb363da7eab56ff0bc28fa8462e0870
unmanbearpig <unmanbearpig@gmail.com>
2014-04-27 13:02:04 +0400

    Added torrent size

--- lib/thepiratebay/fetch.rb
+++ lib/thepiratebay/fetch.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'nokogiri'
 require 'open-uri'
 require 'uri'
@@ -22,18 +23,39 @@ module ThePirateBay
         url         = row.search('.detLink').attribute('href').to_s
         torrent_id  = url.split('/')[2]
 
+        desc_line   = row.search('font.detDesc').children.first.text
+        size = extract_size(desc_line)
+
         torrent = {:title       => title,
                    :seeders     => seeders,
                    :leechers    => leechers,
                    :magnet_link => magnet_link,
                    :category    => category,
                    :torrent_id  => torrent_id,
-                   :url         => url}
+                   :url         => url,
+                   :size        => size}
 
         torrents.push(torrent)
       end
 
       @torrents = torrents
+    end
+
+    private
+
+    def extract_size string
+      match = string.match(/Size ([\d\.]+) ([GM]iB)/)
+      return nil unless match.length == 3
+
+      size = match[1].to_f
+      multiplier = case match[2].downcase
+                   when 'kib' then 1024
+                   when 'mib' then 1048576
+                   when 'gib' then 1073741824
+                   when 'tib' then 1099511627776
+                   else return nil
+                   end
+      return size * multiplier
     end
   end
 end
