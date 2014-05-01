d438bc46ff9ffffaa6541a291bbfee9b4553a08e
unmanbearpig <unmanbearpig@gmail.com>
2014-05-01 23:36:38 +0400

    replaced arguments to search initializer with options hash

--- lib/thepiratebay/search.rb
+++ lib/thepiratebay/search.rb
@@ -5,7 +5,11 @@ module ThePirateBay
     attr_reader :torrents
     alias_method :results, :torrents
 
-    def initialize(query, page = 0, sort_by = 99, category = 0)
+    def initialize(query, options = {})
+      page = options[:page] || 0
+      sort_by = options[:sort_by] || 99
+      category = options[:category] || 0
+
       query = URI.escape(query)
       fetch = Fetch.new 'http://thepiratebay.org/search/' + query + '/' + page.to_s + '/' + sort_by.to_s + '/' + category.to_s + ''
       @torrents = fetch.torrents
